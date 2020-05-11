9a3b0fdd0c0830ab0c4c59b0a15568c393131680
ambatib <30311196+ambatib@users.noreply.github.com>
2020-05-11 09:07:56 +0530

    jlink commands updated

--- src/com/example/jlink/HelloWorld.java
+++ src/com/example/jlink/HelloWorld.java
@@ -9,3 +9,20 @@ public class HelloWorld {
 		LOG.info("Hello World!");
 	}
 }
+
+//--compile module and classes
+//javac -d out module-info.java
+//javac -d out --module-path out com\example\jlink\HelloWorld.java
+//
+//--find dependencies
+//
+//jdeps --module-path out -s --module Java11JlinkExample	
+//
+//--create custom JRE
+//jlink --module-path "%JAVA_HOME%\jmods";out  --add-modules Java11JlinkExample --output C:\customjre
+//
+//--run the program from bin folder of newly created JRE  
+//java --module Java11JlinkExample/com.example.jlink.HelloWorld
+//
+//--create launcher bat file for windows
+//jlink --launcher customjrelauncher=Java11JlinkExample/com.example.jlink.HelloWorld --module-path "%JAVA_HOME%\jmods";out --add-modules Java11JlinkExample --output c:\customjre
